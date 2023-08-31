39e294893069f922181914e8120b4e3a6df285c4
mohamedkhedr700 <M.khedr@rowaad.net>
2023-09-01 01:46:21 +0300

    First init

---
+++ src/Traits/Error/WithErrors.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Raid\Core\Model\Traits\Error;
+
+use Raid\Core\Model\Errors;
+
+trait WithErrors
+{
+    /**
+     * Errors instance.
+     */
+    protected Errors $errors;
+
+    /**
+     * Get the errors handler instance.
+     */
+    public function errors(): Errors
+    {
+        if (! isset($this->errors)) {
+            $this->errors = new Errors();
+        }
+
+        return $this->errors;
+    }
+}
