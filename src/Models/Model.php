16289657aa5e2652f5a316027e9959e970469a5d
mohamedkhedr700 <M.khedr@rowaad.net>
2023-10-20 00:07:47 +0300

    First init

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -25,6 +25,11 @@ class Model extends BaseModel implements ModelInterface
     protected $fillable = [];
 
     /**
+     * Additional cast.
+     */
+    protected array $additionalCast = [];
+
+    /**
      * {@inheritdoc}
      */
     protected static function boot(): void
@@ -63,5 +68,15 @@ class Model extends BaseModel implements ModelInterface
     protected static function fillUpdatedBy(ModelInterface $model): void
     {
         $model->fillAttribute('updated_by', auth()->check() ? auth()?->id() : null);
+    }
+
+    /**
+     * Get the cast array.
+     */
+    public function getCasts(): array
+    {
+        $casts = parent::getCasts();
+
+        return array_merge($casts, $this->additionalCast);
     }
 }
